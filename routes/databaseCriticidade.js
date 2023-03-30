fbe2da3b6b5e565bd205e5a3606e73d92e6a2907
Kathellin Ribeiro <kathellin.ribeiro@gmail.com>
2023-03-29 22:32:16 -0300

    correções model equipamento

--- routes/databaseCriticidade.js
+++ routes/databaseCriticidade.js
@@ -23,6 +23,7 @@ router.post('/post', async (req, res) => {
 router.get('/getAll', async (req, res) => {
     try {
         const data = await Model.find();
+
         res.json(data)
     }
     catch (error) {
